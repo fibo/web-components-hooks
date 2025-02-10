4b755bbe71a8b34a3119096ac8463801204beaa1
Gianluca Casati <fibo@users.noreply.github.com>
2025-02-10 15:00:50 +0700

    setup examples

--- examples/counter.js
+++ examples/counter.js
@@ -1,5 +1,47 @@
 import { defineWebHooksElements } from 'web-hooks';
 
+class CounterValue extends HTMLElement {
+    static observedAttributes = ['data-web-reducer-nonce'];
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        const template = document.createElement('template');
+        template.innerHTML = `<output></output>`;
+        this.shadowRoot?.appendChild(template.content.cloneNode(true));
+        this.output = /** @type {HTMLElement} */ (
+            this.shadowRoot?.querySelector('output')
+        );
+    }
+    connectedCallback() {
+        this.output.textContent = '0';
+    }
+
+    /** @param { string } name */
+    attributeChangedCallback(name) {
+        console.log(name);
+    }
+}
+
+class CounterButton extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        const template = document.createElement('template');
+        template.innerHTML = `<button><slot></slot></button>`;
+        this.shadowRoot?.appendChild(template.content.cloneNode(true));
+    }
+    connectedCallback() {
+        this.shadowRoot
+            ?.querySelector('button')
+            ?.addEventListener('click', () => {
+                const action = this.getAttribute('action');
+                console.log(action);
+            });
+    }
+}
+
 addEventListener('load', () => {
     defineWebHooksElements();
+    customElements.define('counter-button', CounterButton);
+    customElements.define('counter-value', CounterValue);
 });
--- web-hooks.js
+++ web-hooks.js
@@ -1,5 +1,12 @@
 class WebContext extends HTMLElement {
     static localName = 'web-context';
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        const template = document.createElement('template');
+        template.innerHTML = `<slot></slot>`;
+        this.shadowRoot?.appendChild(template.content.cloneNode(true));
+    }
 }
 export function defineWebHooksElements() {
     if (!customElements.get(WebContext.localName))
